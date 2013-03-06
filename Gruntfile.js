a7a4b95c9363465da816347388a41b920b05763a
Martin Auswöger <martin@auswoeger.com>
2013-03-06 10:32:36 +0100

    switched to sass/compass styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,18 @@ module.exports = function(grunt) {
 				dest: '<%= pkg.name %>.min.js'
 			}
 		},
+		compass: {
+			build: {
+				options: {
+					config: 'sass/config.rb',
+					sassDir: 'sass',
+					cssDir: 'css'
+				}
+			}
+		},
 		watch: {
-			files: ['src/**/*', 'lib/**/*'],
-			tasks: ['jshint', 'concat']
+			files: ['src/**/*', 'lib/**/*', 'sass/**/*'],
+			tasks: ['jshint', 'concat', 'compass']
 		},
 		jshint: {
 			files: ['src/Rst/*.js', 'src/jquery-rstSlider.js']
@@ -34,9 +43,10 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-contrib-compass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 
-	grunt.registerTask('default', ['jshint', 'concat', 'min']);
+	grunt.registerTask('default', ['jshint', 'concat', 'min', 'compass']);
 
 };
